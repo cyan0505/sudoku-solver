28de624f12f6cfd6d1eb0c0f181391fe7ee4c85f
Szwajcii <d.szwajkos@gmail.com>
2019-01-22 13:08:48 +0100

    Implement method to parse txt file.

--- src/main/java/com/codecool/sudokusolver/Service/FileParser.java
+++ src/main/java/com/codecool/sudokusolver/Service/FileParser.java
@@ -1,4 +1,41 @@
 package com.codecool.sudokusolver.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileParser {
+
+
+    public int[][] parseFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+        List<String> lines = new ArrayList<>();
+        List<int[]> sudokuBoard = new ArrayList<>();
+        int counter = 0;
+        int[][] board = new int[9][9];
+
+        String line = null;
+
+        while((line = reader.readLine()) != null) {
+
+            String[] row = line.split(",");
+            int[] sudokuRow = Arrays.asList(row).stream().mapToInt(Integer::parseInt).toArray();
+            sudokuBoard.add(sudokuRow);
+
+            for(int i = 0; i < sudokuRow.length; i++){
+                board[counter][i] = sudokuRow[i];
+            }
+
+            counter++;
+        }
+
+        reader.close();
+        return board;
+    }
+
+
 }
