c339a1c6f6c464d11a445d46b9ad7c407f0c71e9
Szwajcii <d.szwajkos@gmail.com>
2019-01-22 15:51:08 +0100

    Implement method to convert multipart file to file. Delete test method to display board.

--- src/main/java/com/codecool/sudokusolver/service/FileParser.java
+++ src/main/java/com/codecool/sudokusolver/service/FileParser.java
@@ -1,22 +1,23 @@
 package com.codecool.sudokusolver.service;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class FileParser {
 
 
-    public int[][] parseFile(String fileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    public int[][] parseFile(MultipartFile multipartFile) throws IOException {
 
+        File file = convertFile(multipartFile);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         List<String> lines = new ArrayList<>();
         int counter = 0;
         int[][] board = new int[9][9];
-
         String line = null;
 
         while((line = reader.readLine()) != null) {
@@ -26,15 +27,19 @@ public class FileParser {
             System.arraycopy(sudokuRow, 0, board[counter], 0, sudokuRow.length);
             counter++;
         }
-
         reader.close();
         return board;
     }
 
 
-    public void display(int[][] table) {
-        for(int[] element : table){
-            System.out.println(Arrays.toString(element));
-        }
+    private File convertFile(MultipartFile file) throws IOException{
+        File convFile = new File(file.getOriginalFilename());
+        convFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
     }
+
+
 }
