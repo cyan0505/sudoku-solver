86a70c6782aaa132e5767079a9e3fbd38edc049d
Szwajcii <d.szwajkos@gmail.com>
2019-01-22 15:53:10 +0100

    Add necessary imports.

--- src/main/java/com/codecool/sudokusolver/service/ISudokuSolver.java
+++ src/main/java/com/codecool/sudokusolver/service/ISudokuSolver.java
@@ -2,6 +2,8 @@ package com.codecool.sudokusolver.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface ISudokuSolver {
-   int[][] solve(MultipartFile file);
+   int[][] solve(MultipartFile file) throws IOException;
 }
