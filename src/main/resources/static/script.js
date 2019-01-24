dd064eac02bb10a15b83627fc56551300209a741
Szwajcii <d.szwajkos@gmail.com>
2019-01-24 14:45:25 +0100

    Implement function to validate upload file type.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,33 +4,19 @@ $(document).ready(
             function(){
                 if ($(this).val()) {
                     $('button:submit').attr('disabled',false);
-                    // or, as has been pointed out elsewhere:
-                    // $('input:submit').removeAttr('disabled');
                 }
             }
         );
     });
 
-$(document).ready(function () {
-    $("#uploadForm").submit(function () {
-        $('#uploadBtn').attr("disabled", true);
-        return true;
-    });
-});
 
-
-// $(document).ready(function () {
-//
-//     $("#uploadForm").submit(function (e) {
-//
-//         //stop submitting the form to see the disabled button effect
-//         e.preventDefault();
-//
-//         //disable the submit button
-//         $("#btnSubmit").attr("disabled", true);
-//
-//
-//         return true;
-//
-//     });
-// });
+function fileValidation() {
+    var fileInput = document.getElementById("exampleFormControlFile1");
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.txt)$/i;
+    if(!allowedExtensions.exec(filePath)) {
+        alert('Please upload txt file only!');
+        fileInput. value = '';
+        return false;
+    }
+}
