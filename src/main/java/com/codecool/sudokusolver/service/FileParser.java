e373f39c64e4b5076a527cce9be7b590517b2544
Czakero <bartosz.pyrz96@gmail.com>
2019-02-12 15:32:51 +0100

    Synchronized list as resturn

--- src/main/java/com/codecool/sudokusolver/service/FileParser.java
+++ src/main/java/com/codecool/sudokusolver/service/FileParser.java
@@ -6,55 +6,35 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class FileParser {
 
-
-//    public int[][] parseFile(MultipartFile multipartFile) throws IOException {
-//
-//        File file = convertFile(multipartFile);
-//        BufferedReader reader = new BufferedReader(new FileReader(file));
-//        List<String> lines = new ArrayList<>();
-//        int counter = 0;
-//        int[][] board = new int[9][9];
-//        String line = null;
-//
-//        while((line = reader.readLine()) != null) {
-//
-//            String[] row = line.split(",");
-//            int[] sudokuRow = Arrays.asList(row).stream().mapToInt(Integer::parseInt).toArray();
-//            System.arraycopy(sudokuRow, 0, board[counter], 0, sudokuRow.length);
-//            counter++;
-//        }
-//        reader.close();
-//        return board;
-//    }
-
-
-    public List<Cell> parseFile(MultipartFile multipartFile) throws IOException {
-
-        File file = convertFile(multipartFile);
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        int rowCounter = 0;
+    public List<Cell> parseFile(MultipartFile multipartFile) {
         List<Cell> cells = new ArrayList<>();
+        try {
+            File file = convertFile(multipartFile);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int rowCounter = 0;
+            cells = new ArrayList<>();
 
-        String line = null;
+            String line;
+            while ((line = reader.readLine()) != null) {
 
-        while((line = reader.readLine()) != null) {
-
-            String[] row = line.split(",");
-            for(int column = 0; column < row.length; column++){
-                cells.add(new Cell(rowCounter, column, Integer.valueOf(row[column])));
+                String[] row = line.split(",");
+                for (int column = 0; column < row.length; column++) {
+                    cells.add(new Cell(rowCounter, column, Integer.valueOf(row[column])));
+                }
+                rowCounter++;
             }
-            rowCounter++;
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        reader.close();
-        return cells;
+        return Collections.synchronizedList(cells);
     }
-
-
 
     private File convertFile(MultipartFile file) throws IOException{
         File convFile = new File(file.getOriginalFilename());
@@ -64,6 +44,4 @@ public class FileParser {
         fos.close();
         return convFile;
     }
-
-
 }
