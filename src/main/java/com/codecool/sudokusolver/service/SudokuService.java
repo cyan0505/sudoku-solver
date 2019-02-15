b1023388a84c742b7671a9b822fe1c15b0e3afae
Czakero <bartosz.pyrz96@gmail.com>
2019-02-15 14:13:17 +0100

    Implemented and overloaded solve method, other methods implemented

--- src/main/java/com/codecool/sudokusolver/service/SudokuService.java
+++ src/main/java/com/codecool/sudokusolver/service/SudokuService.java
@@ -1,13 +1,18 @@
 package com.codecool.sudokusolver.service;
 
+import com.codecool.sudokusolver.model.Sudoku;
+import com.codecool.sudokusolver.model.SudokuResult;
+import com.codecool.sudokusolver.model.SudokuSolverManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 @Service
-public class SudokuService {
+public class SudokuService implements ISudokuSolver{
 
     private FileParser fileParser;
 
@@ -16,17 +21,33 @@ public class SudokuService {
         this.fileParser = fileParser;
     }
 
-
-    public int[][] solveSudoku(MultipartFile multipartFile) throws IOException, InterruptedException {
-
-//        Sudoku sudoku = new Sudoku(fileParser.parseFile(multipartFile));
-
-//        sudoku.solve();
-
-        return null;
-
+    @Override
+    public SudokuResult solve(MultipartFile file) throws IOException, ExecutionException, InterruptedException {
+        Sudoku sudoku = fileParser.parseFile(file);
+        long starttime = System.currentTimeMillis();
+        Sudoku solvedSudoku = SudokuSolverManager.solve(sudoku);
+        long endtime = System.currentTimeMillis();
+        return new SudokuResult(SudokuUtils.convertSudokuToIntArray(solvedSudoku), starttime - endtime);
     }
 
+    @Override
+    public SudokuResult solve(int[] cellsValues) throws ExecutionException, InterruptedException {
+        long starttime = System.currentTimeMillis();
+        Sudoku solvedSudoku = SudokuSolverManager.solve(SudokuUtils.convertIntArrayToSudoku(cellsValues));
+        long endtime = System.currentTimeMillis();
 
+        return new SudokuResult(SudokuUtils.convertSudokuToIntArray(solvedSudoku), starttime - endtime);
+    }
 
+    @Override
+    public int[][] uploadBoard(MultipartFile file) throws IOException {
+        Sudoku sudoku = fileParser.parseFile(file);
+        return SudokuUtils.convertSudokuToIntArray(sudoku);
+    }
+
+    @Override
+    public int[][] uploadExampleBoard(File file) throws IOException {
+        Sudoku sudoku = fileParser.parseFile(file);
+        return SudokuUtils.convertSudokuToIntArray(sudoku);
+    }
 }
