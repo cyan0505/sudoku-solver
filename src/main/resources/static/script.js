66cff2fbebe6b0aa94cdf22131a08ee0d1f016e7
Czakero <bartosz.pyrz96@gmail.com>
2019-02-15 21:57:59 +0100

    Var to let refactor, function for setting value of parent element after changing value in cell to make it able to pass values to backend implemented, validation for input values implemented, function parsing values and sending them to backend implemented

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -11,9 +11,9 @@ $(document).ready(
 
 
 function fileValidation() {
-    var fileInput = document.getElementById("exampleFormControlFile1");
-    var filePath = fileInput.value;
-    var allowedExtensions = /(\.txt)$/i;
+    let fileInput = document.getElementById("exampleFormControlFile1");
+    let filePath = fileInput.value;
+    let allowedExtensions = /(\.txt)$/i;
     if(!allowedExtensions.exec(filePath)) {
         alert('Please upload txt file only!');
         fileInput. value = '';
@@ -22,23 +22,46 @@ function fileValidation() {
 }
 
 function getAllCells() {
-
-    var sudokuCellList = [];
-
-    for (let i = 1; i <= 81; i++) {
-        let cell = document.getElementById("cell" + i);
-        sudokuCellList.push(cell.value);
+    let tds = $('[data-cell="cell"]');
+    let sudokuCellList = [];
+    let arrayLength = tds.length;
+    for (let i = 0; i < arrayLength; i++) {
+        let value = tds[i].getAttribute("value");
+        if (value === ' ' || value === null) {
+            value = 0;
+        }
+        sudokuCellList.push(new CellDTO(value));
     }
 
     let httpRequest = new XMLHttpRequest();
 
     httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        if(httpRequest.readyState === 4 && httpRequest.status === 200) {
             $("html").html(httpRequest.responseText);
         }
     };
     let json = "{\"sudokuCells\": " + JSON.stringify(sudokuCellList) + "}";
-    console.log(json);
-    httpRequest.open('POST', '/userGrid');
+    httpRequest.open('POST', '/solve');
     httpRequest.send(json);
+}
+
+$(":input").keyup(function(){
+    var input = $(this).val();
+    var regex = new RegExp("^[1-9]$");
+    if(regex.test(input) || input === "") {
+    } else {
+        alert("Please enter only number in range 1 - 9.");
+        $(this).val(input.substring(0,input.length-1));
+    }
+});
+
+$('input').on('change keydown keyup',function () {
+    let parent = $(this).parent();
+    parent.get(0).setAttribute("value", $(this).val());
+});
+
+class CellDTO {
+    constructor(value) {
+        this.value = value;
+    }
 }
