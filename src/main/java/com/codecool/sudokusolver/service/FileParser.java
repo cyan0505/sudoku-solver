47f7e2ba12facb6b93f38ca1196942a8219c84f4
Czakero <bartosz.pyrz96@gmail.com>
2019-02-15 14:12:43 +0100

    Updated file parser to return domain model rather than components of model

--- src/main/java/com/codecool/sudokusolver/service/FileParser.java
+++ src/main/java/com/codecool/sudokusolver/service/FileParser.java
@@ -1,6 +1,7 @@
 package com.codecool.sudokusolver.service;
 
 import com.codecool.sudokusolver.model.Cell;
+import com.codecool.sudokusolver.model.Sudoku;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,37 +9,58 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class FileParser {
+    public Sudoku parseFile(int[] cells) {
+        List<Cell> cellList = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                cellList.add(new Cell(i, j, cells[index++]));
 
-    public List<Cell> parseFile(MultipartFile multipartFile) {
-        List<Cell> cells = new ArrayList<>();
-        try {
-            File file = convertFile(multipartFile);
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            int rowCounter = 0;
-            cells = new ArrayList<>();
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-
-                String[] row = line.split(",");
-                for (int column = 0; column < row.length; column++) {
-                    cells.add(new Cell(rowCounter, column, Integer.valueOf(row[column])));
-                }
-                rowCounter++;
             }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return Collections.synchronizedList(cells);
+        return new Sudoku(cellList);
     }
 
-    private File convertFile(MultipartFile file) throws IOException{
-        File convFile = new File(file.getOriginalFilename());
-        convFile.createNewFile();
+    public Sudoku parseFile(MultipartFile multipartFile) throws IOException {
+
+        File file = convertFile(multipartFile);
+        List<Cell> cells = fromFileToCells(file);
+
+        return new Sudoku(Collections.synchronizedList(cells));
+    }
+
+    private List<Cell> fromFileToCells(File file) throws IOException {
+        List<Cell> cells;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        int rowCounter = 0;
+        cells = new ArrayList<>();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+
+            String[] row = line.split(",");
+            for (int column = 0; column < row.length; column++) {
+                cells.add(new Cell(rowCounter, column, Integer.valueOf(row[column])));
+            }
+            rowCounter++;
+        }
+        reader.close();
+        return cells;
+    }
+
+    public Sudoku parseFile(File file) throws IOException {
+
+        List<Cell> cells = fromFileToCells(file);
+
+        return new Sudoku(Collections.synchronizedList(cells));
+    }
+
+    private File convertFile(MultipartFile file) throws IOException {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
         FileOutputStream fos = new FileOutputStream(convFile);
         fos.write(file.getBytes());
         fos.close();
