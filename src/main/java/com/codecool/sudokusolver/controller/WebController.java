2d639f29141acea26849fb9a98d49bd10b45c885
Czakero <bartosz.pyrz96@gmail.com>
2019-02-15 14:13:39 +0100

    Controller refactoring, not done

--- src/main/java/com/codecool/sudokusolver/controller/WebController.java
+++ src/main/java/com/codecool/sudokusolver/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.codecool.sudokusolver.controller;
 
+import com.codecool.sudokusolver.model.SudokuResult;
 import com.codecool.sudokusolver.service.ISudokuSolver;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.Arrays;
+
 
 @Controller
 public class WebController {
     private ISudokuSolver sudokuSolver;
-    private String fileName;
-    private String[][] manualGrid;
-    private String[] userGrid;
 
     @Autowired
     public WebController(ISudokuSolver sudokuSolver) {
@@ -29,66 +30,60 @@ public class WebController {
 
 
     @PostMapping("/solver")
-    public String handleSolvedSudoku(Model model) throws IOException {
-        model.addAttribute("solvedSudoku", sudokuSolver.solve());
-        model.addAttribute("fileName", this.fileName);
-        model.addAttribute("time", sudokuSolver.elapsedTime());
+    public String handleSolvedSudoku(@RequestParam MultipartFile file, Model model) {
+        SudokuResult sudokuResult;
+        try {
+            sudokuResult = sudokuSolver.solve(file);
+        } catch (Exception e) {
+            return "error";
+        }
+        model.addAttribute("solvedSudoku", sudokuResult.getSudoku());
+        model.addAttribute("fileName", file.getName());
+        model.addAttribute("time", sudokuResult.getElapsedTime());
         return "result";
     }
 
-
-    @PostMapping("/sudoku")
-    public String handleSudokuBoard(@RequestParam MultipartFile file, Model model) throws IOException {
-        this.fileName = file.getOriginalFilename();
-        model.addAttribute("sudoku", sudokuSolver.uploadBoard(file));
-        model.addAttribute("fileName", fileName);
+    @PostMapping("/upload")
+    public String uploadHandler(@RequestParam MultipartFile file, Model model) {
+        try {
+            model.addAttribute("sudoku", sudokuSolver.uploadBoard(file));
+            model.addAttribute("fileName", file.getOriginalFilename());
+        } catch (IOException e) {
+            return "error";
+        }
         return "sudoku";
     }
 
     @PostMapping("/userGrid")
-    public String handleManualGrid(HttpEntity<String> request, Model model) throws IOException {
+    public String handleManualGrid(HttpEntity<String> request, Model model) {
 
         Gson g = new Gson();
         String json = request.getBody();
         System.out.println("JSON :" + json);
-        SudokuCellList sudokuCells = g.fromJson(json, SudokuCellList.class);
-        System.out.println("\n\nSudoku: " + sudokuCells);
+        int[] sudokuCells = g.fromJson(json, (Type) Array.class);
 
-
-        String[][] userGrid = sudokuSolver.getUserGrid(sudokuCells);
-
-        for(int i = 0; i < userGrid.length; i++){
-            System.out.println(Arrays.toString(userGrid[i]));
+        System.out.println("\n\nSudoku: " + Arrays.toString(sudokuCells));
+        SudokuResult sudokuResult;
+        try {
+            sudokuResult = sudokuSolver.solve(sudokuCells);
+        } catch (Exception e) {
+            return "error";
         }
 
-        sudokuSolver.generateUserGrid(userGrid);
-        model.addAttribute("solvedSudoku", sudokuSolver.solve());
-        model.addAttribute("time", sudokuSolver.elapsedTime());
-        System.out.println("WESZLO!!!!");
+        model.addAttribute("solvedSudoku", sudokuResult.getSudoku());
+        model.addAttribute("time", sudokuResult.getElapsedTime());
         return "result";
     }
 
 
     @PostMapping("/example")
-    public String handleExampleSudokuBoard(@RequestParam("grid") String grid, Model model) throws IOException {
-        File file = null;
-
-        this.fileName = grid;
-
-        if (grid.equals("Grid one")) {
-            file = new File("src/main/resources/sudoku/grid1.txt");
-        } else if (grid.equals("Grid two")) {
-            file = new File("src/main/resources/sudoku/grid2.txt");
-        } else if (grid.equals("Grid three")) {
-            file = new File("src/main/resources/sudoku/grid3.txt");
-        } else if (grid.equals("Grid four")) {
-            file = new File("src/main/resources/sudoku/grid4.txt");
-        } else if (grid.equals("Worlds hardest sudoku")) {
-            file = new File("src/main/resources/sudoku/worlds_hardest_sudoku.txt");
+    public String handleExampleSudokuBoard(@RequestParam("grid") String grid, Model model)  {
+        File file = new File("src/main/resources/sudoku/" + grid);
+        try {
+            model.addAttribute("sudoku", sudokuSolver.uploadExampleBoard(file));
+        } catch (IOException e) {
+            return "error";
         }
-
-        model.addAttribute("sudoku", sudokuSolver.uploadExampleBoard(file));
-        model.addAttribute("fileName", fileName);
         return "example";
     }
 
